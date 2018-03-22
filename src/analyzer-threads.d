528fa67d130596ad18c52f006af322e4f0b1c973
Randy Caldejon <rc@counterflowai.com>
2018-03-22 03:19:28 -0400

    Add unit test #4

--- src/analyzer-threads.d
+++
@@ -1,3 +0,0 @@
-analyzer-threads.o: analyzer-threads.c analyzer-threads.h suricata-cmds.h \
- dragonfly-cmds.h /usr/include/lua5.1/lua.h /usr/include/lua5.1/lauxlib.h \
- /usr/include/lua5.1/lua.h /usr/include/lua5.1/lualib.h dragonfly-io.h
