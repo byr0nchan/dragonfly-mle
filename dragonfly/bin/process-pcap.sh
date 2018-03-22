c68aefd11ba4608fbc9aace1bff4464561054252
Randy Caldejon <rc@counterflowai.com>
2018-03-21 20:16:26 -0400

    Added two more unit test cases.

--- dragonfly/bin/process-pcap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to automate the processing of pcap files with Suricata
-#
-SURI_ROOT="/opt/suricata/"
-suricatasc  -c "pcap-file /home/td/Downloads/pcaps/vault1.pcap /tmp" ${SURI_ROOT}/var/run/suricata/suricata-command.socket 
-
-
--- src/dragonfly.d
+++
@@ -1,4 +0,0 @@
-dragonfly.o: dragonfly.c /usr/include/lua5.1/lua.h \
- /usr/include/lua5.1/luaconf.h /usr/include/lua5.1/lauxlib.h \
- /usr/include/lua5.1/lua.h /usr/include/lua5.1/lualib.h suri_io.h \
- dragonfly.h
