9acfbe4b7197fecf434ad9ecb89f6cd89307aacd
Randy Caldejon <rc@counterflowai.com>
2018-03-28 18:58:19 -0400

    Integrated lua-cjson into the main application.

---
+++ src/lua-cjson.h
@@ -0,0 +1,25 @@
+/*
+ * 
+ * Copyright (C) 2017-2018 CounterFlow AI, Inc.
+ * ===============================================================================
+ * 
+ * This is a forked, modified version of lua-hiredis.  See the following for 
+ * additional information and orginal license:
+ * 
+ * https://github.com/mpx/lua-cjson
+ * 
+ * ===============================================================================
+ */
+/* 
+ *
+ * author Randy Caldejon <rc@counterflowai.com>
+ *
+ */
+
+#ifndef _LUA_CJSON_
+#define _LUA_CJSON_
+
+LUALIB_API int luaopen_cjson_safe(lua_State * L);
+
+#endif
+
