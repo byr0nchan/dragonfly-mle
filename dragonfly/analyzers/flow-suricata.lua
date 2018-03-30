2c9b4fbb92d4a8b7b7e24b83216d03cdeb9ab15a
Randy Caldejon <rc@counterflowai.com>
2018-03-30 09:44:17 -0400

    1) Implemented lua/c functiont to convert iso8602 date to epoch 2) Implemented another lua/redis command to allow pure text commands 3) Implemented flow analyzer that utilizes Random Forest ML

---
+++ dragonfly/analyzers/flow-suricata.lua
@@ -0,0 +1,59 @@
+-- ----------------------------------------------
+-- Flow analysis using Random Forest ML
+-- ----------------------------------------------
+local open = io.open
+
+local mlpath = "analyzers/flow-suricata.ml"
+
+-- ----------------------------------------------
+--
+-- ----------------------------------------------
+function setup()
+    conn = hiredis.connect()
+    assert(conn:command("PING") == hiredis.status.PONG)
+    
+    print ("Reading model......")
+    local file = open(mlpath, "rb") -- r read mode and b binary mode
+    local model = file:read ("*a")
+    file:close()
+
+    reply = conn:command_line(model)
+    print ("Loaded ML model: ", reply)
+
+end
+
+
+-- ----------------------------------------------
+--
+-- ----------------------------------------------
+function loop(msg)
+    command = {}
+    local eve = cjson.decode(msg)
+    if eve and eve.event_type == 'flow' and eve.flow.age then
+        -- print (msg)
+
+        -- extract the fields
+        total_pkts = eve.flow.pkts_toclient + eve.flow.pkts_toserver
+        total_bytes = eve.flow.bytes_toclient + eve.flow.bytes_toserver
+        duration = eve.flow.age
+        src_port = eve.src_port
+        dest_port = eve.dest_port
+
+        -- generate the ML redis command
+        table.insert(command, "ML.FOREST.RUN")
+        table.insert(command, "5-raw-plus-num-v17:tree")
+
+        table.insert(command, "tot_pkts:"..total_pkts)
+        table.insert(command, "tot_bytes:"..total_bytes)
+        table.insert(command, "duration:"..duration)
+        table.insert(command, "src_port_num:"..src_port)
+        table.insert(command, "dest_port_num:"..dest_port)
+        table.insert(command, "CLASSIFICATION")
+
+        local ml_command = table.concat(command, " ")
+        print (ml_command)    
+        reply = conn:command_line(ml_command)
+	    print (reply)	    
+    end
+end
+
